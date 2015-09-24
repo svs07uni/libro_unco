b71633048d3ad60b5a5c47e2223d8702064c8a3a
svs07uni <svs07uni@gmail.com>
2015-09-24 11:01:28 -0300

    Version 2 - libro_unco en funcionamiento

--- php/plan_de_estudio/ci_datos_plan.php
+++ php/plan_de_estudio/ci_datos_plan.php
@@ -3,6 +3,9 @@ class ci_datos_plan extends ci_plan_de_estudio
 {
     protected $s__id_sede;
     protected $s__nuevo_plan;
+    protected $s__areas;
+    
+    protected $s__datos_filtro;
         //-----------------------------------------------------------------------------------
 	//---- pant_datos -------------------------------------------------------------------
 	//-----------------------------------------------------------------------------------
@@ -12,90 +15,102 @@ class ci_datos_plan extends ci_plan_de_estudio
 
 	function conf__form_plan(libro_unco_ei_formulario $form)
 	{
-            
             if(isset($this->controlador->s__id_plan)){//Existen datos de este plan
+                $filtro['id_plan']['valor'] = $this->controlador->s__id_plan;
                 $ar = $this->controlador()->dep('datos')->tabla('plan_estudio')->get();
-                $ar['titulo'] = utf8_decode($ar['titulo']);
-                $ar['nombre'] = utf8_decode($ar['nombre']);
+                if(!isset($ar)){//No hay nada cargado en memoria ent buscar en BD, en el caso que se presionó cancelar
+                    $plan = $this->controlador()->dep('datos')->tabla('plan_estudio')->get_listado($filtro);
+                    if(count($plan)>0){
+                        $ar = $plan[0];
+                        $p['id_plan'] = $plan[0]['id_plan'];
+                        $this->controlador()->dep('datos')->tabla('plan_estudio')->cargar($p);
+                    } 
+                    $pertenece = $this->controlador()->dep('datos')->tabla('pertenece')->get_listado($filtro);
+                    foreach($pertenece as $clave => $valor){
+                        $areas[$clave] = $valor['id_area'];
+                    }
+                    $this->s__areas = $areas;
+                }                 
+                $ar['areas'] = $this->s__areas;
+                
                 return $ar;
             }
-            
+            else{//Es plan nuevo, oculto las pantallas y el evento guardar propio del controlador
+                $this->pantalla()->tab('pant_sede')->ocultar();
+                $this->pantalla()->tab('pant_observacion')->ocultar();
+                $this->pantalla()->tab('pant_modulos')->ocultar();
+                $this->pantalla()->tab('pant_materias')->ocultar();
+                $this->pantalla()->tab('pant_descripcion')->ocultar();
+                $this->evento('guardar')->ocultar();
+                $this->evento('cancelar')->ocultar();
+                $this->evento('listo')->ocultar();
+            }            
 	}
 
 	function evt__form_plan__guardar($datos)
-	{
-            $datos['titulo'] = utf8_encode($datos['titulo']);
-            $datos['nombre'] = utf8_encode($datos['nombre']);
-            $datos['id_unidad_academica'] = $this->s__sigla;
+	{             
+            $this->s__areas = $datos['areas'];
+            $datos['id_unidad_academica'] = $this->controlador->s__sigla;
             $this->controlador()->dep('datos')->tabla('plan_estudio')->set($datos);
             $ar = $this->controlador()->dep('datos')->tabla('plan_estudio')->get();
-            if(!isset($ar['id_plan'])){//No Existe el campo id_plan ent no existe este registro
-                $this->controlador()->dep('datos')->tabla('plan_estudio')->sincronizar();
-                $ar = $this->controlador()->dep('datos')->tabla('plan_estudio')->get();
-                $this->controlador->s__id_plan = $ar['id_plan'];
-                $this->s__nuevo_plan = true;//Como no existia ent si presiona cancelar este registro se debe borrar
-            }
-            
+            $this->controlador->s__id_plan = $ar['id_plan'];            
 	}
+        
+        function evt__form_plan__crear($datos){
+            $datos['id_unidad_academica'] = $this->controlador->s__sigla;
+            $this->controlador()->dep('datos')->tabla('plan_estudio')->set($datos);
+            $this->controlador()->dep('datos')->tabla('plan_estudio')->sincronizar();
+            
+            $ar = $this->controlador()->dep('datos')->tabla('plan_estudio')->get();
+            $this->controlador->s__id_plan = $ar['id_plan'];
+            
+        }
+        
+        function evt__form_plan__cancelar(){
+            $this->controlador()->dep('datos')->tabla('plan_estudio')->resetear();
+            $this->controlador()->set_pantalla('pant_planes');
+        }
 
 	//-----------------------------------------------------------------------------------
 	//---- pant_sede --------------------------------------------------------------------
 	//-----------------------------------------------------------------------------------
+        
         //-----------------------------------------------------------------------------------
 	//---- form_sede -------------------------------------------------------------------
 	//-----------------------------------------------------------------------------------
 
 	function conf__form_sede(libro_unco_ei_formulario $form){//Si ingresa a este form ent ya existe plan y está cargado
             
-            $sede = $this->controlador()->dep('datos')->tabla('sede')->get();
+            $sede = $this->controlador()->dep('datos')->tabla('sede')->get();//print_r($sede);
             if(!isset($sede)){//No existe sede cargada en memoria
                 //Se busca en la base de datos  
                 $filtro['id_plan']['valor'] = $this->controlador->s__id_plan;
                 $sede = $this->controlador()->dep('datos')->tabla('sede')->get_listado($filtro);
-                if(count($sede)>0)//Existe en la BD
+                if(count($sede)>0){//Existe en la BD
                     $sede = $sede[0];
+                    $s['id_sede'] = $sede['id_sede'];
+                    $this->controlador()->dep('datos')->tabla('sede')->cargar($s);
+                }
             }
         
-            if(count($sede)>0){//Si existe cargada en la memoria o en la BD
-                $sede['direccion_calle'] = utf8_decode($sede['direccion_calle']);
-                $fp_imagen = $this->controlador()->dep('datos')->tabla('sede')->get_blob('imagen_plano_referencial');
-                    
-                if (isset($fp_imagen)) {
-                    //-- Se necesita el path fisico y la url de una archivo temporal que va a contener la imagen
-                    $temp_nombre = 'img'.uniqid(time()).'.jpg';
-                    $temp_archivo = toba::proyecto()->get_www_temp($temp_nombre);
-	
-                    //-- Se pasa el contenido al archivo temporal
-                    $temp_fp = fopen($temp_archivo['path'], 'w');
-                    stream_copy_to_stream($fp_imagen, $temp_fp);
-                    fclose($temp_fp);
-                    $tamaño = round(filesize($temp_archivo['path']) / 1024);
-		  	
-                    //-- Se muestra la imagen temporal
-                    $sede['imagen_vista_previa'] = "<img src='{$temp_archivo['url']}' alt='' width=100 height=100>";
-                    $sede['imagen'] = 'Tamaño: '.$tamaño. ' KB';
-                        
-                 } else {
-                    $sede['imagen'] = null;
-                        
-                 }
+            if(isset($sede)){//Encontre en memoria o BD
+                if(isset($sede['id_localidad'])){
+                    $filtro['id_localidad']['valor'] = $sede['id_localidad'];
+                    $localidad = $this->controlador()->dep('datos')->tabla('localidad')->get_listado($filtro);
+                    $sede['id_provincia'] = $localidad[0]['id_provincia'];
+                }
+//$sede['maps']="<iframe src='https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d96159.42063655656!2d-71.3399145!3d-41.1349212!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1442344288208' width='200' height='200' frameborder='0' style='border:0' allowfullscreen></iframe>";
                 return $sede;
             }
         }
                 
 	function evt__form_sede__guardar($datos)
-	{print_r($datos);
-            $datos['direccion_calle'] = utf8_encode($datos['direccion_calle']);
+	{
             $datos['id_plan'] = $this->controlador->s__id_plan;
             $this->controlador()->dep('datos')->tabla('sede')->set($datos);
-            //$this->s__id_sede = $this->controlador()->dep('datos')->tabla('sede')->get();
-            if (is_array($datos['imagen_plano_referencial'])) {
-                //Se subio una imagen
-                $fp = fopen($datos['imagen_plano_referencial']['tmp_name'], 'rb');
-                $this->controlador()->dep('datos')->tabla('sede')->set_blob('imagen_plano_referencial', $fp);
-            } 
             
         }
+        
 
 	//-----------------------------------------------------------------------------------
 	//---- pant_observacion -------------------------------------------------------------------
@@ -103,7 +118,7 @@ class ci_datos_plan extends ci_plan_de_estudio
         //-----------------------------------------------------------------------------------
 	//---- form_observacion -------------------------------------------------------------------
 	//-----------------------------------------------------------------------------------
-
+        
 	function conf__form_observacion(libro_unco_ei_formulario $form)
 	{//Ya existe el plan cargado
             $obs = $this->controlador()->dep('datos')->tabla('obs_plan')->get();
@@ -123,9 +138,9 @@ class ci_datos_plan extends ci_plan_de_estudio
 
 	function evt__form_observacion__guardar($datos)
 	{
-            $datos['descripcion'] = utf8_encode($datos['descripcion']);
-            $datos['id_entidad'] = $this->controlador->s__id_plan;
-            $this->controlador()->dep('datos')->tabla('obs_plan')->set($datos);
+                $datos['descripcion'] = utf8_encode($datos['descripcion']);
+                $datos['id_entidad'] = $this->controlador->s__id_plan;
+                $this->controlador()->dep('datos')->tabla('obs_plan')->set($datos);
             
 	}
         
@@ -160,12 +175,8 @@ class ci_datos_plan extends ci_plan_de_estudio
 	function conf__cuadro_modulos(libro_unco_ei_cuadro $cuadro)
 	{
             $this->s__datos_filtro['id_plan']['valor'] = $this->controlador->s__id_plan;
-            if(isset($this->s__datos_filtro)){
-                $cuadro->set_datos($this->controlador()->dep('datos')->tabla('modulo')->get_listado($this->s__datos_filtro));
-            }
-            else{
-                $cuadro->set_datos($this->controlador()->dep('datos')->tabla('modulo')->get_listado());
-            }
+            $cuadro->set_datos($this->controlador()->dep('datos')->tabla('modulo')->get_listado($this->s__datos_filtro));
+            
 	}
 
 	function evt__cuadro_modulos__seleccion($seleccion)
@@ -223,10 +234,9 @@ class ci_datos_plan extends ci_plan_de_estudio
             if(isset($this->s__datos_filtro)){
                 $datos = $this->controlador()->dep('datos')->tabla('materia')->get_listado($this->s__datos_filtro);
             }
-            else{
-                $datos = $this->controlador()->dep('datos')->tabla('materia')->get_listado();
+            for($i=0; $i<sizeof($datos); $i++){
+                $datos[0]['optativa'] = $datos[0]['optativa']?"Sí":"No";               
             }
-            print_r($datos);
             return $datos;
 	}
 
@@ -323,52 +333,44 @@ class ci_datos_plan extends ci_plan_de_estudio
         function evt__guardar(){
             try{
                 $this->controlador()->dep('datos')->tabla('plan_estudio')->sincronizar();
+                $sql = "DELETE FROM pertenece WHERE id_plan = ".$this->controlador->s__id_plan;
+                toba::db('libro_unco')->consultar($sql);
+                foreach($this->s__areas as $clave => $valor){
+                    $pertenece = array();
+                    $pertenece['id_plan'] = $this->controlador->s__id_plan;
+                    $pertenece['id_area'] = $valor;
+                    $this->controlador()->dep('datos')->tabla('pertenece')->set($pertenece);
+                    $this->controlador()->dep('datos')->tabla('pertenece')->sincronizar();
+                    $this->controlador()->dep('datos')->tabla('pertenece')->resetear();
+                }
+            
             }catch(toba_error_validacion $e){
-                $this->set_pantalla('pant_plan');
+                
             }
             try{
                 $this->controlador()->dep('datos')->tabla('sede')->sincronizar();
             }catch(toba_error_validacion $e){
-                $this->set_pantalla('pant_sede');
+                
             }
             try{
                 $this->controlador()->dep('datos')->tabla('obs_plan')->sincronizar();
             }catch(toba_error_validacion $e){
-                $this->set_pantalla('pant_observacion');
-            }
-            try{
-                $this->controlador()->dep('datos')->tabla('materia')->sincronizar();
-            }catch(toba_error_validacion $e){
                 
             }
-            try{
-                $this->controlador()->dep('datos')->tabla('obs_mat')->sincronizar();
-            }catch(toba_error_validacion $e){
-                
-            }
-            try{
-                $this->controlador()->dep('datos')->tabla('modulo')->sincronizar();
-            }catch(toba_error_validacion $e){
-                
-            }
-            try{
-                $this->controlador()->dep('datos')->tabla('obs_mod')->sincronizar();
-            }catch(toba_error_validacion $e){
-                
-            }
+            
         }
         
         function evt__cancelar(){
-            if($this->s__nuevo_plan){
-                    $sql = "delete from plan_estudio where id_plan = '".$this->controlador->s__id_plan."'";
-                    toba::db('libro_unco')->consultar($sql);
-            }
-
-            $this->disparar_limpieza_memoria();
+            $this->controlador()->dep('datos')->tabla('plan_estudio')->resetear();
+            $this->controlador()->dep('datos')->tabla('sede')->resetear();
+            $this->controlador()->dep('datos')->tabla('obs_plan')->resetear();
         }
         
         function evt__listo(){
-            $this->controlador()->dep('datos')->resetear();
+            $this->controlador()->dep('datos')->tabla('plan_estudio')->resetear();
+            $this->controlador()->dep('datos')->tabla('sede')->resetear();
+            $this->controlador()->dep('datos')->tabla('obs_plan')->resetear();
+            $this->controlador->s__id_plan = null;
             $this->controlador()->set_pantalla('pant_planes');
         }
     
